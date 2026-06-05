c56ad44756a255305180a56e9d458ebd7a65838b
Annolla Tulasi <tulasi.tls.111@gmail.com>
2026-06-05 10:51:34 +0530

    enhancements

--- news-api-email/main.py
+++ news-api-email/main.py
@@ -3,13 +3,15 @@ import requests
 from send_email import send_email
 
 api_key = "6e7f4ef989d1456f977963dce9abbde0"
-url =  "https://newsapi.org/v2/everything?q=tesla&from=2026-05-04&sortBy=publishedAt&apiKey=6e7f4ef989d1456f977963dce9abbde0"
+url =  "https://newsapi.org/v2/everything?"\
+       "q=tesla&sortBy=publishedAt&apiKey=6e7f4ef989d1456f977963dce9abbde0&language=en"
 
 request = requests.get(url)
 content = request.json()
+#print(content)
 body = " "
-for article in content["articles"]:
-    body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
-print(body)
+for article in content["articles"][:20]:
+    body = "Subject: Todays news" + "\n" + body + article["title"] + "\n" + str(article["description"]) + "\n" + article["url"] + 2*"\n"
+#print(body)
 body = body.encode("utf-8")
 send_email(body)

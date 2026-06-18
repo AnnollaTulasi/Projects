907f40580ab3961d4aa773400ddb0df6483e76e8
Annolla Tulasi <tulasi.tls.111@gmail.com>
2026-06-18 21:42:11 +0530

    flask

---
+++ flask-app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+
+app = Flask("Website")
+
+@app.route("/")
+def home():
+    return render_template("tutorial.html")
+
+@app.route("/api/v1/<station>/<date>")
+def temp(station,date):
+    return {"station": station,
+            "date": date,
+            "temp": 0}
+
+if __name__ == "__main__":
+    app.run(debug=True)

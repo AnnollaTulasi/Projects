e61057f810a256c7956cea5e18a6758e374655c7
Annolla Tulasi <tulasi.tls.111@gmail.com>
2026-06-22 05:47:00 +0530

    flask app temperature based on date and id

--- flask-app/main.py
+++ flask-app/main.py
@@ -1,6 +1,8 @@
+from fileinput import filename
+import pandas as pd
 from flask import Flask,render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -8,9 +10,13 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def temp(station,date):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    print(filename)
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
-            "temp": 0}
+            "temp": temperature}
 
 if __name__ == "__main__":
     app.run(debug=True)

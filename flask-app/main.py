58309bf37ec7075710e7c6d71efb5da979ea3d22
Annolla Tulasi <tulasi.tls.111@gmail.com>
2026-06-23 10:22:53 +0530

    show stanid and staname on the home page

--- flask-app/main.py
+++ flask-app/main.py
@@ -12,12 +12,24 @@ def home():
 @app.route("/api/v1/<station>/<date>")
 def temp(station,date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
-    print(filename)
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temp": temperature}
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    return df.to_dict(orient="records")
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station,year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    return df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
 
 if __name__ == "__main__":
     app.run(debug=True)

f36831bca81b48618f9f886f4a1ed19948608ce3
Annolla Tulasi <tulasi.tls.111@gmail.com>
2026-06-23 21:14:12 +0530

    getting temp,sky details from api

--- Weather-Forecast-Dashboard/main.py
+++ Weather-Forecast-Dashboard/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place:")
@@ -7,6 +8,8 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view ", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} in {place}")
+
+data = get_data(place, days, option)
 
 dates = ["2026-21-06","2026-22-06","2026-23-06"]
 temperatures = [10, 11, 15]
